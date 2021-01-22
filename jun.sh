0b50dac4958017edc68193b368a753de74799103
Jun <jun@junaire.com>
2021-01-22 14:27:30 +0800

    initial commit

---
+++ jun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+umask 007
+
+PUBLIC_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDt01uJaUK35vZVgmc3QL8k2TYLeHJRFEOTlItdDbDZh6Yuc4bLw3Y0qhPEovzRlNEDdI5LzxpwQCBCIdHhpb5Jtrc3T4+bAogxnkEHWRYMPusrdbHwEr1JE6XiuD282qD0RE2PsNq5rg9lt66IqdjaHwHfk8CbOn9EQu1BaBIVAbIFd074x8COP8xsQRTjNuOZv3wmeoPyn4Xugj9tmotH/EUGEian7t3SNxkqQ0BVOxZJPGYf9tmRiyymci6MLKf+t+OVjqXqJFfkN2QEvJVl837COzjstlTItuMDKApPCSRn7OXrB/FG9WWd5/4nEKIIDEoB/EVhftw3MUVtofvR jun@junaire.com'
+
+is_root(){
+	[ "${EUID}" -ne 0 ] && echo "The script must be ran as root!" && exit 1
+}
+
+config_ssh() {
+	if [ -d "~/.ssh" ] ;
+	then
+		mkdir -p ~/.ssh
+	fi
+
+	[ -z "${PUBLIC_KEY}" ] && echo "Please configure your ssh public key!" && exit 1
+	echo "${PUBLIC_KEY}" > ~/.ssh/authorized_keys
+
+	sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+	sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+
+	systemctl restart sshd.service
+}
+
+is_root
+config_ssh
